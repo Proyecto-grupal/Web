e0df5c93d094573809fb3ed92a2888898050b9e3
Ignacio Amatt <ignacio_amatt@hotmail.com>
2021-08-30 00:04:09 -0300

    Deploy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg'
 import './App.css'
 
+const { REACT_APP_ENV } = process.env
+
 function App () {
   return (
     <div className='App'>
@@ -15,7 +17,7 @@ function App () {
           target='_blank'
           rel='noopener noreferrer'
         >
-          Learn React
+          {REACT_APP_ENV}
         </a>
       </header>
     </div>
