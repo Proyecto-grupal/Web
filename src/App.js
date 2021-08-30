af624174cfc99053ccae71b60b0ebf756495e12b
Ignacio Amatt <ignacio_amatt@hotmail.com>
2021-08-30 01:01:35 -0300

    Testing Deploy con variables de entorno OK

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
-import logo from './logo.svg'
-import './App.css'
-
-// const functions = require('firebase-functions')
-// const request = require('request-promise')
-
-const { REACT_APP_ENV } = process.env
+import { Location } from './config/config'
 
 function App () {
-  console.log(REACT_APP_ENV)
-
   return (
     <div className='App'>
       <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          {REACT_APP_ENV}
-        </a>
+
+        {Location}
+
       </header>
     </div>
   )
---
+++ src/config/config.js
@@ -0,0 +1,1 @@
+export const Location = process.env.NODE_ENV === 'development' ? 'Estoy en Desarrollo' : 'Estoy en Producción'
